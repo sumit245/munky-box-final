d6274a210e8d87401ae80a8e97d60c833934a5bc
sumit245 <sumitranjan245@gmail.com>
2022-01-01 04:30:34 +0530

    Message

--- src/scenes/Components/ratings/Rate.js
+++ src/scenes/Components/ratings/Rate.js
@@ -9,7 +9,8 @@ import {
   SafeAreaView,
 } from "react-native";
 import { AirbnbRating } from "react-native-elements";
-import { Chip } from "react-native-paper";
+import { Chip, Provider } from "react-native-paper";
+import { Actions } from "react-native-router-flux";
 import Loader from "../utility/Loader";
 import SuccessDialog from "../utility/SuccessDialog";
 
@@ -52,11 +53,12 @@ export default function Rate({ navigation, restaurant, order }) {
       "http://munkybox-admin.herokuapp.com/api/review/",
       review
     );
-    const { status } = response.data;
+    const { data } = response;
     setLoaded(false);
-    if (status === 200) {
-      console.log("success");
+    console.log(data);
+    if (data !== null) {
       setPlaced(true);
+      Actions.pop();
     }
   };
   const features = [
@@ -68,131 +70,142 @@ export default function Rate({ navigation, restaurant, order }) {
   ];
   if (!loaded) {
     return (
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: "space-between",
-          backgroundColor: "#fff",
-        }}
-      >
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text
-              style={{
-                textAlign: "center",
-                fontWeight: "bold",
-                fontSize: 20,
-                letterSpacing: 1,
-              }}
-            >
-              Thanks for rating!
-            </Text>
-            <Text style={{ textAlign: "center", fontWeight: "bold" }}>
-              Order {order.order_id}
-            </Text>
-          </View>
-          <AirbnbRating
-            reviews={["Worst", "Bad", "Good", "Satisfied", "Excellent!!!"]}
-            onFinishRating={changeRating}
-          />
+      <Provider>
+        <SafeAreaView
+          style={{
+            flex: 1,
+            justifyContent: "space-between",
+            backgroundColor: "#fff",
+          }}
+        >
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: 20,
+                  letterSpacing: 1,
+                }}
+              >
+                Thanks for rating!
+              </Text>
+              <Text style={{ textAlign: "center", fontWeight: "bold" }}>
+                Order {order.order_id}
+              </Text>
+            </View>
+            <AirbnbRating
+              reviews={["Worst", "Bad", "Good", "Satisfied", "Excellent!!!"]}
+              onFinishRating={changeRating}
+            />
 
-          <View
-            style={{
-              marginHorizontal: 2,
-              marginVertical: 24,
-              marginTop: 48,
-            }}
-          >
-            <Text
-              style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}
-            >
-              What did you like?{" "}
-            </Text>
             <View
               style={{
-                flexDirection: "row",
-                maxWidth: "90%",
-                justifyContent: "space-between",
-                flexWrap: "wrap",
-                marginHorizontal: 20,
-                marginVertical: 2,
+                marginHorizontal: 2,
+                marginVertical: 24,
+                marginTop: 48,
               }}
             >
-              {features.map((feature, key) => (
-                <Chip
-                  mode="outlined"
-                  onPress={() => pushToLiked(feature)}
-                  selectedColor={likes.includes(feature) ? "#2962ff" : "#000"}
-                  style={{
-                    minWidth: 100,
-                    height: 40,
-                    marginVertical: 2,
-                    borderWidth: 2,
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  textStyle={{
-                    textAlign: "center",
-                    fontSize: 14,
-                    color: "#000",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {feature}
-                </Chip>
-              ))}
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                }}
+              >
+                What did you like?{" "}
+              </Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  maxWidth: "90%",
+                  justifyContent: "space-between",
+                  flexWrap: "wrap",
+                  marginHorizontal: 20,
+                  marginVertical: 2,
+                }}
+              >
+                {features.map((feature, key) => (
+                  <Chip
+                    mode="outlined"
+                    onPress={() => pushToLiked(feature)}
+                    selectedColor={likes.includes(feature) ? "#2962ff" : "#000"}
+                    style={{
+                      minWidth: 100,
+                      height: 40,
+                      marginVertical: 2,
+                      borderWidth: 2,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                    textStyle={{
+                      textAlign: "center",
+                      fontSize: 14,
+                      color: "#000",
+                      fontWeight: "bold",
+                    }}
+                    key={key}
+                  >
+                    {feature}
+                  </Chip>
+                ))}
+              </View>
             </View>
-          </View>
 
-          <View>
-            <Text
-              style={{ textAlign: "center", fontSize: 16, fontWeight: "bold" }}
-            >
-              Leave a comment
-            </Text>
-            <TextInput
-              style={{
-                height: 120,
-                margin: 12,
-                borderWidth: 1,
-                padding: 10,
-                borderRadius: 4,
-                textAlignVertical: "top",
-              }}
-              numberOfLines={4}
-              placeholder="Write any comments"
-              onChangeText={(text) => setDetails(text)}
-            />
-          </View>
-        </ScrollView>
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 16,
+                  fontWeight: "bold",
+                }}
+              >
+                Leave a comment
+              </Text>
+              <TextInput
+                style={{
+                  height: 120,
+                  margin: 12,
+                  borderWidth: 1,
+                  padding: 10,
+                  borderRadius: 4,
+                  textAlignVertical: "top",
+                }}
+                numberOfLines={4}
+                placeholder="Write any comments"
+                onChangeText={(text) => setDetails(text)}
+              />
+            </View>
+          </ScrollView>
 
-        <TouchableOpacity
-          style={{
-            width: "98%",
-            borderRadius: 6,
-            borderWidth: 0.2,
-            marginHorizontal: 2,
-            padding: 6,
-            height: 44,
-            alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: "#A91B60",
-          }}
-          onPress={onSubmit}
-        >
-          <Text
+          <TouchableOpacity
             style={{
-              color: "#fff",
-              textTransform: "uppercase",
-              fontWeight: "bold",
-              fontSize: 20,
+              width: "98%",
+              borderRadius: 6,
+              borderWidth: 0.2,
+              marginHorizontal: 2,
+              padding: 6,
+              height: 44,
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#A91B60",
             }}
+            onPress={onSubmit}
           >
-            Submit
-          </Text>
-        </TouchableOpacity>
-        {placed && <SuccessDialog />}
-      </SafeAreaView>
+            <Text
+              style={{
+                color: "#fff",
+                textTransform: "uppercase",
+                fontWeight: "bold",
+                fontSize: 20,
+              }}
+            >
+              Submit
+            </Text>
+          </TouchableOpacity>
+          {placed && <SuccessDialog />}
+        </SafeAreaView>
+      </Provider>
     );
   } else {
     return <Loader />;
