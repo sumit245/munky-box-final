74a5793d48d226ba10a2dc15d2125ba165f4f528
sumit245 <sumitranjan245@gmail.com>
2022-01-25 12:44:06 +0530

    Message

--- src/scenes/Components/emaillogin/EmailLogin.js
+++ src/scenes/Components/emaillogin/EmailLogin.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Text, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/Fontisto";
+import Icon from "react-native-vector-icons/Ionicons";
 import styles from "../../styles/AuthStyle";
 import { width } from "../../styles/HomeStyles";
 
 export default function EmailLogin() {
   return (
     <TouchableOpacity style={[styles.btnOTP, { width: width - 48,alignSelf:'center' }]}>
-      <Icon name="email" color="#4267B2" size={26} />
+      <Icon name="keypad-outline" color="#4267B2" size={26} />
       <Text style={{ fontSize: 20, fontWeight: "bold" }}>
         Continue with PIN
       </Text>
