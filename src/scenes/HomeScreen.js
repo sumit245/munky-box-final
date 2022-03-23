0b35db210ef817f27bf08b62d69b11af126a7b8b
sumit245 <sumitranjan245@gmail.com>
2022-03-23 13:00:09 +0530

    message

--- src/scenes/HomeScreen.js
+++ src/scenes/HomeScreen.js
@@ -34,7 +34,7 @@ export default function HomeScreen({ navigation }) {
             ),
 
             tabBarLabel: "Home",
-            ta,
+            
           }}
         />
         <Tab.Screen
