6bbe867daf38241102e76fed03bbae4e6c76fe98
sumit245 <sumitranjan245@gmail.com>
2022-03-29 05:48:45 +0530

    message

--- src/scenes/Components/Logo.js
+++ src/scenes/Components/Logo.js
@@ -11,7 +11,7 @@ export default class Logo extends Component {
             justifyContent: "center",
           }}
         >
-<Image source={LogoImage} height={60} width={60} resizeMode="contain" style={{height:60,width:60}} />
+<Image source={LogoImage} height={60} width={60} resizeMode="contain" style={{height:120,width:120}} />
         </View>
     );
   }
