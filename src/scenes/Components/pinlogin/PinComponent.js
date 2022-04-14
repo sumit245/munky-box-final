7f3efa67d9a1dc3af335e28494b4d23573d3715e
sumit245 <sumitranjan245@gmail.com>
2022-04-14 13:16:27 +0530

    message

--- src/scenes/Components/pinlogin/PinComponent.js
+++ src/scenes/Components/pinlogin/PinComponent.js
@@ -1,9 +1,10 @@
 import Icon from "react-native-vector-icons/Ionicons";
 import React, { useEffect, useRef, useState } from "react";
-import { ImageBackground, View, SafeAreaView, Text } from "react-native";
+import { ImageBackground, View, SafeAreaView, Text,StyleSheet,Platform } from "react-native";
 import ReactNativePinView from "react-native-pin-view";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import BackButton from "../../../scenes/Components/utility/BackButton"
+import { height, width } from "../../styles/HomeStyles";
 
 const PinComponent = ({ navigation, entry, logintype, data }) => {
   const pinView = useRef(null);
@@ -161,4 +162,119 @@ const PinComponent = ({ navigation, entry, logintype, data }) => {
     </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(25,25,25,0.45)",
+    width: width,
+    alignItems: "center",
+  },
+  image: {
+    marginTop: 60,
+    marginBottom: 120,
+    height: "auto",
+    width: "auto",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  TextInput: {
+    textAlign: "left",
+    borderBottomWidth: 1,
+    width: "90%",
+    height: 45,
+    marginBottom: 20,
+  },
+  forgot_button: {
+    color: "#FFF",
+    marginTop: 20,
+    bottom: Platform.OS === "ios" ? -120 : 0,
+    fontWeight: "bold",
+    fontSize: 16,
+    textDecorationLine: "underline",
+  },
+  phoneContainer: {
+    height: 50,
+    width: "96%",
+    borderColor: "black",
+    borderWidth: 1,
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginVertical: 6,
+  },
+  loginBtn: {
+    width: "96%",
+    borderRadius: 10,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 6,
+  },
+  btnText: {
+    color: "#ffffff",
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#FFF",
+    textTransform: "uppercase",
+  },
+  errormsg: {
+    color: "#cf6c22",
+  },
+  imageBackground: {
+    width: width,
+    flex: 1,
+    alignItems: "center",
+  },
+  mobin: {
+    marginTop: height / 2 - 200,
+    alignItems: "center",
+    flex: 1,
+  },
+  instructions: {
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+    marginTop: 6,
+  },
+  textInputContainer: {
+    marginTop: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  roundedTextInput: {
+    height: 40,
+    width: 40,
+    borderRadius: 6,
+    backgroundColor: "#fff",
+    borderWidth: 0.1,
+  },
+  pinMsg: {
+    fontWeight: "bold",
+    color: "#FFF",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  pinMsgView: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor: "rgba(100,100,100,0.5)",
+    paddingHorizontal: 20,
+    width: 296,
+    padding: 10,
+  },
+  pinInputAreaStyle: {
+    marginBottom: 24,
+    backgroundColor: "rgba(100,100,100,0.5)",
+    paddingHorizontal: 60,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    paddingBottom: 10,
+  },
+});
+
+
 export default PinComponent;
