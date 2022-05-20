f93310c760e5c91a0f2585e5b638b7f95f928354
sumit245 <sumitranjan245@gmail.com>
2022-05-20 12:53:43 +0530

    message

--- src/scenes/Components/checkout/PromoOptions.js
+++ src/scenes/Components/checkout/PromoOptions.js
@@ -108,7 +108,7 @@ export default class PromoOptions extends Component {
         </View>
         {pulled && (
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+            
           >
             {
               isAdmin && (
