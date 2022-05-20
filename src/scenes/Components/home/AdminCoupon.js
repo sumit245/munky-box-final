54a021b699f430251ff2d558fe0bf3c3c836966b
sumit245 <sumitranjan245@gmail.com>
2022-05-20 10:52:37 +0530

    message

--- src/scenes/Components/home/AdminCoupon.js
+++ src/scenes/Components/home/AdminCoupon.js
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import MarqueeText from "react-native-marquee"
 import axios from 'axios'
 export default function () {
